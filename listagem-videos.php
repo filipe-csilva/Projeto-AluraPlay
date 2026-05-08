c491391e90f6bde9b29ea4c2215bb4b475cf5934
Filipe Silva <81836461+filipe-csilva@users.noreply.github.com>
2026-05-07 21:47:56 -0300

    Atualização de projeto

--- listagem-videos.php
+++ listagem-videos.php
@@ -1,62 +1,26 @@
 <?php
-    require ("src/conexao-bd.php");
-    require ("src/Repository/VideoRepository.php");
 
-    $videoRepositorio = new VideoRepository($pdo);
-    $dadosVideos = $videoRepositorio->buscarTodos();
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO("sqlite:$dbPath");
+$videoList = $pdo->query('SELECT * FROM videos;')->fetchAll(\PDO::FETCH_ASSOC);
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./css/reset.css">
-    <link rel="stylesheet" href="./css/estilos.css">
-    <link rel="stylesheet" href="./css/flexbox.css">
-    <title>AluraPlay</title>
-    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
-</head>
-
-<body>
-    <header>
-
-        <nav class="cabecalho">
-            <a class="logo" href="/"></a>
-
-            <div class="cabecalho__icones">
-                <a href="./pages/novo-video" class="cabecalho__videos"></a>
-                <a href="./pages/login" class="cabecalho__sair">Sair</a>
+<?php require_once 'inicio-html.php'; ?>
+    <ul class="videos__container">
+        <?php foreach ($videoList as $video): ?>
+        <li class="videos__item">
+            <iframe width="100%" height="72%" src="<?= $video['url']; ?>"
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+            <div class="descricao-video">
+                <h3><?= $video['title']; ?></h3>
+                <div class="acoes-video">
+                    <a href="/editar-video?id=<?= $video['id']; ?>">Editar</a>
+                    <a href="/remover-video?id=<?= $video['id']; ?>">Excluir</a>
+                </div>
             </div>
-        </nav>
-
-    </header>
-
-    <ul class="videos__container" alt="videos alura">
-        <?php foreach($dadosVideos as $video):?>
-            <?php if(str_starts_with($video->getUrl(), 'http')):?>
-                <li class="videos__item">
-                    <iframe width="100%" height="72%" src="<?= $video->getUrl() ?>"
-                        title="YouTube video player" frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen></iframe>
-                    <div class="descricao-video">
-                        <img src="./img/logo.png" alt="logo canal alura">
-                        <h3><?= $video->getTitle() ?></h3>
-                        <div class="acoes-video">
-                            <a href="./pages/editar-video.php?id=<?= $video->getId() ?>">Editar</a>
-                            <a href="./pages/remove-video.php?id=<?= $video->getId() ?>">Excluir</a>
-                        </div>
-                    </div>
-                </li>
-            <?php endif;?>
+        </li>
         <?php endforeach; ?>
     </ul>
-</body>
-
-</html>
+<?php require_once 'fim-html.php'; ?>
--- pages/remove-video.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    require ("../src/conexao-bd.php"); 
-    require ("../src/Model/Video.php");
-    require ("../src/Repository/VideoRepository.php");
-
-    $videoRepositorio = new VideoRepository($pdo);
-    $videoRepositorio->removeVideo($_GET["id"]);
-    header('Location: /');
